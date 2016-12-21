ad641fa546400270bb183b58db7a3fe0050f3193
timcartwright <tim@websitesbytim.com>
2016-12-21 17:27:15 +0000

    Update galleries

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 ;(function($) {
+    // Initialise galleries
+    $(document).ready(function() {
+        $("#lightgallery").lightGallery(); 
+        $("#lightgallerytwo").lightGallery(); 
+        $("#lightgallerythree").lightGallery(); 
+        $("#lightgallerylook").lightGallery({
+            // animateThumb: false,
+            // enableThumbDrag: false,
+            // enableThumbSwipe: false
+        }); 
+    });
     // Colour image on hover
     $('.hide').hover(function() {
         $(this).prev('img.grayscale').toggleClass('remove-grayscale');
@@ -9,10 +20,14 @@
         var target = '.' + this.id;
 
         $('[class^=toggle-]:not(' + target + ')').hide();
-        $('img.grayscale').removeClass('color');
 
-        $(target).slideToggle('slow');
-        $(this).toggleClass('current');
+        $(target).slideToggle('slow', function() {
+            $('img.grayscale').removeClass('color');
+            $(this).toggleClass('current');
+            $(target).toggleClass('open');
+            $('body').toggleClass('content-open');
+            $(target).prepend('<a id="close"><img class="close-button" src="img/close.png" /></a>');
+        });
 
         if ($('.toggleaction').hasClass('current')) {
             $('img.remove-grayscale').addClass('color');
@@ -21,5 +36,16 @@
         $('html, body').animate({
             scrollTop: $(target).offset().top - 100
         }, 'slow');
+    });
+
+    // Close content
+    $('.overlay').on('click', function() {
+        $('body').removeClass('content-open');
+        $('.open').slideToggle('slow', function() {
+            $('img.grayscale').removeClass('color');
+            $('.current').removeClass('current');
+            $('.open').removeClass('open');
+            $('#close').remove();
+        });
     });
 })(window.jQuery);
